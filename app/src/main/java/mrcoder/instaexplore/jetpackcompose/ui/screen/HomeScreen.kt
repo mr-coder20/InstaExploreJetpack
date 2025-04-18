3fb4357c49a5981dfbffdcc95193ef86767c44c6
mr-coder20 <amirhosseinghafari29@gmail.com>
2025-04-18 20:58:54 +0330

    fix bug

---
+++ app/src/main/java/mrcoder/instaexplore/jetpackcompose/ui/screen/HomeScreen.kt
@@ -0,0 +1,15 @@
+
+package mrcoder.instaexplore.jetpackcompose.ui.screen
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen() {
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("Home Page")
+    }
+}
---
+++ app/src/main/java/mrcoder/instaexplore/jetpackcompose/viewmodel/AppStateViewModel.kt
@@ -0,0 +1,35 @@
+package mrcoder.instaexplore.jetpackcompose.viewmodel
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import mrcoder.instaexplore.jetpackcompose.UiState
+
+
+class AppStateViewModel(
+    private val photoViewModel: PhotoViewModel
+) : ViewModel() {
+
+    // استفاده از StateFlow برای مدیریت وضعیت
+    private val _uiState = MutableStateFlow<UiState<Unit>>(UiState.Loading)
+    val uiState: StateFlow<UiState<Unit>> = _uiState
+
+    init {
+        preloadApp()
+    }
+
+    private fun preloadApp() {
+        viewModelScope.launch {
+            try {
+                // شبیه‌سازی بارگذاری داده‌ها
+                photoViewModel.fetchPhotos()
+                _uiState.value = UiState.Success(Unit)
+            } catch (e: Exception) {
+                _uiState.value = UiState.Error("خطا در بارگذاری داده‌ها: ${e.message}")
+            }
+        }
+    }
+}
